faa815b854e048dc5edd7f60f7d1546c2a11daf9
Ash2003SE <afashraful37@gmail.com>
2025-06-08 01:07:21 +0300

    Processor

---
+++ Master/Processor.cs
@@ -0,0 +1,56 @@
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+using Shared;
+
+public class Processor
+{
+    private readonly string pipe1, pipe2;
+    private readonly List<WordEntry> allData = new();
+    private readonly object lockObj = new();
+
+    public Processor(string p1, string p2)
+    {
+        pipe1 = p1;
+        pipe2 = p2;
+    }
+
+    public void Run()
+    {
+        var t1 = new Thread(() => Listen(pipe1));
+        var t2 = new Thread(() => Listen(pipe2));
+
+        t1.Start();
+        t2.Start();
+        t1.Join();
+        t2.Join();
+
+        DisplayResult();
+    }
+
+    private void Listen(string pipeName)
+    {
+        using var server = new NamedPipeServerStream(pipeName, PipeDirection.In);
+        Console.WriteLine($"Waiting for {pipeName}...");
+        server.WaitForConnection();
+
+        using var reader = new StreamReader(server);
+        var json = reader.ReadLine() ?? "[]";
+        var entries = PipeUtils.Deserialize(json);
+
+        lock (lockObj)
+        {
+            allData.AddRange(entries);
+        }
+
+        Console.WriteLine($"Received from {pipeName}");
+    }
+
+    private void DisplayResult()
+    {
+        foreach (var entry in allData)
+        {
+            Console.WriteLine($"{entry.FileName}:{entry.Word}:{entry.Count}");
+        }
+    }
+}
